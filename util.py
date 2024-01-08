ad7ec6d4693cb8a860ecb773044aa7df8da131a3
kenttcrane <crane4869@yahoo.co.jp>
2024-01-09 01:27:53 +0900

    show_messageの仕様変更

--- util.py
+++ util.py
@@ -4,10 +4,11 @@ from tkinter import ttk
 def show_message(msg):
     root = tk.Tk()
     root.overrideredirect(True)
-    root.wait_visibility(root)
-    root.attributes('-alpha', 0.7)
 
     label=tk.Label(root, text=msg, font=('', 36, 'normal'))
     label.pack(anchor='center')
+
     root.after(400, lambda: root.destroy()) 
+    root.wait_visibility(root)
+    root.attributes('-alpha', 0.7)
     root.mainloop()
